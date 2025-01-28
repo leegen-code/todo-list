8824dd9dfec75813c3cb1b690dcf3219c7ef6576
leegen-code <186605203+leegen-code@users.noreply.github.com>
2025-01-28 19:35:40 -0100

    add static file serving and update HTML structure for todo list

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.get('/', (req, res) => {
   res.sendFile(PUBLIC_HTML + '/' + 'todolist.html');
 });
 
+app.use(express.static('public'));
+
 app.listen(port, () => {
   console.log(`To-do-list app listening at http://localhost:${port}`);
 });

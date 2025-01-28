f59cd48e2548c684eeaabcf29bef03f5d1f573bf
leegen-code <186605203+leegen-code@users.noreply.github.com>
2025-01-28 19:30:04 -0100

    add basic server setup and HTML file for to-do list application

---
+++ app.js
@@ -0,0 +1,13 @@
+const PUBLIC_HTML = __dirname + '/public/html';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(PUBLIC_HTML + '/' + 'todolist.html');
+});
+
+app.listen(port, () => {
+  console.log(`To-do-list app listening at http://localhost:${port}`);
+});
